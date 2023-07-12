62c7c9e5cb362bcaf0f19b8b46934559af6fa919
YeHoonJang <jangyh0420@gmail.com>
2023-07-12 17:00:23 +0900

    upload alpaca-lora

---
+++ alpaca-lora/custom_loss_train.sh
@@ -0,0 +1,10 @@
+python finetune_custom_loss.py \
+--base_model 'decapoda-research/llama-7b-hf' \
+--data_path "data/ko_alpaca_data.json" \
+--output_dir "./outputs/llama_7b_koalpaca_custom_loss" \
+--num_epochs 3 \
+--learning_rate 5e-4 \
+--val_set_size 2000 \
+--batch_size 8 \
+--micro_batch_size 1 \
+--prompt_template_name "custom"
