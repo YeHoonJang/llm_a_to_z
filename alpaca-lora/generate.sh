6c05b865f2fcfc57376b1f86f263790b15fc1f9f
YeHoonJang <jangyh0420@gmail.com>
2023-07-12 17:32:01 +0900

    Rearrange: alpaca-lora files & directories

--- alpaca-lora/generate.sh
+++
@@ -1,5 +0,0 @@
-python generate.py \
---base_model "decapoda-research/llama-7b-hf" \
---lora_weights "./outputs/llama_7b_koalpaca_temp" \
---prompt_template "custom" \
---share_gradio
---
+++ alpaca-lora/shell_script/generate.sh
@@ -0,0 +1,5 @@
+python generate.py \
+--base_model "decapoda-research/llama-7b-hf" \
+--lora_weights "./outputs/llama_7b_koalpaca_temp" \
+--prompt_template "custom" \
+--share_gradio
