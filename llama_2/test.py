b415e14a29c1f760fe0fc24df125bca257d33076
yehoon <jangyh0420@gmail.com>
2023-08-03 15:42:18 +0900

    Move into archive

--- llama_2/test.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.utils.data import Dataset, DataLoader, ConcatDataset
-
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", use_auth_token=True)
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", use_auth_token=True)
