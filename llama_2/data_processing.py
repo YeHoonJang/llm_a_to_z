30fc71cea3ea0caf1028721ae09f51d0a54016d3
yehoon <jangyh0420@gmail.com>
2023-09-14 21:20:03 +0900

    Edit intro blurb of HF benchmark's

--- llama_2/data_processing.py
+++ llama_2/data_processing.py
@@ -61,7 +61,8 @@ def create_prompt_formats(opt, sample):
         # options = "\n".join([" ".join([str(idx), text]) for idx, text in enumerate(sample["endings"])])
         options = [" ".join([str(idx), text]) for idx, text in enumerate(sample["endings"])]
 
-        blurb = f"{INTRO_BLURB} Choose the number to continue the sentence in context and complete it appropriately. The answer must be one of the one in the list. Answer with the number of the correct answer without any explanations and quotation mark."
+        # blurb = f"{INTRO_BLURB} The answer must be one of the one in the list. Answer with the number or symbol of the correct answer without any explanations and quotation mark."
+        blurb = f"{INTRO_BLURB}"
         instruction = f"{INSTRUCTION_KEY}\n{sample['ctx']}\n\n{str(options)}"
         input_context = ""
         response = f"{RESPONSE_KEY}\n{sample['label']}"
@@ -70,7 +71,8 @@ def create_prompt_formats(opt, sample):
         # options = options = "\n".join([" ".join([str(idx), text]) for idx, text in enumerate(sample["choices"])])
         options = options = [" ".join([str(idx), text]) for idx, text in enumerate(sample["choices"])]
 
-        blurb = f"{INTRO_BLURB} The answer must be one of the one in the list. Answer with the number of the correct answer without any explanations and quotation mark."
+        # blurb = f"{INTRO_BLURB} The answer must be one of the one in the list. Answer with the number or symbol of the correct answer without any explanations and quotation mark."
+        blurb = f"{INTRO_BLURB}"
         instruction = f"{INSTRUCTION_KEY}\n{sample['question']}\n\n{str(options)}"
         input_context = ""
         response = f"{RESPONSE_KEY}\n{sample['answer']}"
@@ -79,7 +81,8 @@ def create_prompt_formats(opt, sample):
         # options = "\n".join([" ".join([str(idx), text]) for idx, text in enumerate(sample["mc1_targets"]["choices"])])
         options = [" ".join([str(idx), text]) for idx, text in enumerate(sample["mc1_targets"]["choices"])]
 
-        blurb = f"{INTRO_BLURB} The answer must be one of the one in the list. Answer with the number or symbol of the correct answer without any explanations and quotation mark."
+        # blurb = f"{INTRO_BLURB} The answer must be one of the one in the list. Answer with the number or symbol of the correct answer without any explanations and quotation mark."
+        blurb = f"{INTRO_BLURB}"
         instruction = f"{INSTRUCTION_KEY}\n{sample['question']}\n\n{str(options)}"
         input_context = ""
         response = f"{RESPONSE_KEY}\n{sample['mc1_targets']['labels'].index(1)}"
